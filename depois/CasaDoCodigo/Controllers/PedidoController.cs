86d15a64491dc73600f7fdbc17d78c419d2e1995
marcelooliveira <mclricardo@gmail.com>
2019-03-19 18:45:16 -0300

    .

---
+++ depois/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,80 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+        private readonly IPedidoRepository pedidoRepository;
+        private readonly IItemPedidoRepository itemPedidoRepository;
+
+        public PedidoController(IProdutoRepository produtoRepository,
+            IPedidoRepository pedidoRepository,
+            IItemPedidoRepository itemPedidoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+            this.pedidoRepository = pedidoRepository;
+            this.itemPedidoRepository = itemPedidoRepository;
+        }
+
+        public async Task<IActionResult> Carrossel()
+        {
+            return View(await produtoRepository.GetProdutosAsync());
+        }
+
+        public async Task<IActionResult> BuscaProdutos(string pesquisa)
+        {
+            return View(await produtoRepository.GetProdutosAsync(pesquisa));
+        }
+
+        public async Task<IActionResult> Carrinho(string codigo)
+        {
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                await pedidoRepository.AddItemAsync(codigo);
+            }
+
+            var pedido = await pedidoRepository.GetPedidoAsync();
+            List<ItemPedido> itens = pedido.Itens;
+            CarrinhoViewModel carrinhoViewModel = new CarrinhoViewModel(itens);
+            return base.View(carrinhoViewModel);
+        }
+
+        public async Task<IActionResult> Cadastro()
+        {
+            var pedido = await pedidoRepository.GetPedidoAsync();
+
+            if (pedido == null)
+            {
+                return RedirectToAction("Carrossel");
+            }
+
+            return View(pedido.Cadastro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Resumo(Cadastro cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(await pedidoRepository.UpdateCadastroAsync(cadastro));
+            }
+            return RedirectToAction("Cadastro");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<UpdateQuantidadeResponse> UpdateQuantidade([FromBody]ItemPedido itemPedido)
+        {
+            return await pedidoRepository.UpdateQuantidadeAsync(itemPedido);
+        }
+    }
+}
---
+++ depois/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,72 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Task SaveProdutosAsync(List<Livro> livros);
+        Task<IList<Produto>> GetProdutosAsync();
+        Task<BuscaProdutosViewModel> GetProdutosAsync(string pesquisa);
+    }
+
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+    {
+        private readonly ICategoriaRepository categoriaRepository;
+
+        public ProdutoRepository(ApplicationContext contexto,
+            ICategoriaRepository categoriaRepository) : base(contexto)
+        {
+            this.categoriaRepository = categoriaRepository;
+        }
+
+        public async Task<IList<Produto>> GetProdutosAsync()
+        {
+            return await dbSet
+                .Include(prod => prod.Categoria)
+                .ToListAsync();
+        }
+
+        public async Task<BuscaProdutosViewModel> GetProdutosAsync(string pesquisa)
+        {
+            IQueryable<Produto> query = dbSet;
+
+            if (!string.IsNullOrEmpty(pesquisa))
+            {
+                query = query.Where(q => q.Nome.Contains(pesquisa));
+            }
+
+            query = query
+                .Include(prod => prod.Categoria);
+
+            return new BuscaProdutosViewModel(await query.ToListAsync(), pesquisa);
+        }
+
+        public async Task SaveProdutosAsync(List<Livro> livros)
+        {
+            foreach (var livro in livros)
+            {
+                var categoria = await categoriaRepository.AddCategoriaAsync(livro.Categoria);
+
+                if (!await dbSet.Where(p => p.Codigo == livro.Codigo).AnyAsync())
+                {
+                    await dbSet.AddAsync(new Produto(livro.Codigo, livro.Nome, livro.Preco, categoria));
+                }
+            }
+            await contexto.SaveChangesAsync();
+        }
+    }
+
+    public class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Subcategoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
