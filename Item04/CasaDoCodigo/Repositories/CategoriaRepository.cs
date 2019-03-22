f3f1723afb221878bdf2b058c5d6e4793d43e15c
marcelooliveira <mclricardo@gmail.com>
2019-03-22 01:23:00 -0300

    Item04

---
+++ Item04/CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,35 @@
+﻿using CasaDoCodigo.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task<Categoria> AddCategoriaAsync(string categoriaNome);
+    }
+
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public async Task<Categoria> AddCategoriaAsync(string categoriaNome)
+        {
+            var categoriaDB =
+                dbSet
+                    .Where(c => c.Nome == categoriaNome)
+                    .SingleOrDefault();
+
+            if (categoriaDB == null)
+            {
+                categoriaDB = new Categoria(categoriaNome);
+                await dbSet.AddAsync(categoriaDB);
+                await contexto.SaveChangesAsync();
+            }
+            return categoriaDB;
+        }
+
+    }
+}
